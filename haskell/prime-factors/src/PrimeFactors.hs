0a587c8dd93a1681f2d5063f05fcca8aa7c01e05
Timothy Manu Meixell <misternu@gmail.com>
2019-01-09 14:08:33 -0500

    Revert "Finish rust and haskell"

--- haskell/prime-factors/src/PrimeFactors.hs
+++ haskell/prime-factors/src/PrimeFactors.hs
@@ -1,10 +1,4 @@
 module PrimeFactors (primeFactors) where
 
 primeFactors :: Integer -> [Integer]
-primeFactors x = primeFactors' x 2
-
-primeFactors' :: Integer -> Integer -> [Integer]
-primeFactors' x c
-  | (x < c) = []
-  | (x `mod` c == 0) = [c] ++ primeFactors' (x `div` c) c
-  | otherwise = primeFactors' x (c + 1)
+primeFactors x = []
