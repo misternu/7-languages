085bca20ae3939edd772ce8e9fab49a7ba68b09f
Timothy Meixell <misternu@gmail.com>
2019-01-09 19:37:20 -0500

    finish golang

--- go/src/prime_factors/prime_factors.go
+++ go/src/prime_factors/prime_factors.go
@@ -1,5 +1,11 @@
 package prime_factors
 
 func Factors(num int) []int {
-  return []int{}
+  factors := []int{}
+  for c := 2; c <= num; c++ {
+    for ; num % c == 0; num /= c {
+      factors = append(factors, c)
+    }
+  }
+  return factors
 }
