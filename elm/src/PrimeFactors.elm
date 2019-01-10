9450e793a185db63b06227e9a1a4ad716ffa62ee
Timothy Meixell <misternu@gmail.com>
2019-01-09 20:19:05 -0500

    finish elm

--- elm/src/PrimeFactors.elm
+++ elm/src/PrimeFactors.elm
@@ -1,4 +1,11 @@
 module PrimeFactors exposing (..)
 
 primeFactors : Int -> (List Int)
-primeFactors x = []
+primeFactors x = primeFactorsPrime x 2
+
+primeFactorsPrime : Int -> Int -> (List Int)
+primeFactorsPrime x c =
+  if x < 2 then []
+  else if remainderBy c x == 0 then
+    [c] ++ primeFactorsPrime (x // c) c
+  else primeFactorsPrime x (c + 1)
