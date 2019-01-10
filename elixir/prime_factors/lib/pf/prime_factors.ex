d88a13407278e21c5888f0ca7e0da444f54ff4fd
Timothy Meixell <misternu@gmail.com>
2019-01-09 23:32:05 -0500

    finish elixir

--- elixir/prime_factors/lib/pf/prime_factors.ex
+++ elixir/prime_factors/lib/pf/prime_factors.ex
@@ -1,5 +1,10 @@
 defmodule PF.PrimeFactors do
-  def prime_factors(num) do
-    []
+  def prime_factors(num, c \\ 2) do
+    cond do
+      num == 1 -> []
+      rem(num, c) == 0 ->
+        [c] ++ prime_factors(div(num, c), c)
+      true -> prime_factors(num, c + 1)
+    end
   end
 end
